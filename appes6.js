daab8f4a58d101f0379bb2a7380667339825591f
davidagina <davidagina64@gmail.com>
2022-05-10 16:32:34 +0100

    local storage functionality

--- appes6.js
+++ appes6.js
@@ -56,6 +56,54 @@ class UI{
     }
 }
 
+// Local Storage
+class Store {
+    static getBooks(){
+        let books;
+        if (localStorage.getItem('books') === null){
+            books = []
+        } else {
+            books = JSON.parse(localStorage.getItem('books')) 
+        }
+
+        return books
+    }
+
+    static displayBooks(){
+        const books = Store.getBooks();
+
+        books.forEach(function(book){
+            const ui = new UI();
+
+            // Add book to UI
+            ui.addBookToList(book)
+        })
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books))
+    }
+
+    static removeBook(isbn){
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1)
+            }
+        })
+
+        localStorage.setItem('books', JSON.stringify(books))
+    }
+}
+
+// DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks)
+
 // Event Listeners to add book
 document.getElementById('book-form').addEventListener('submit', function(e){
     // Get form values
@@ -77,6 +125,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     //  Add book
     ui.addBookToList(book);
 
+    // Add to Local Storage
+    Store.addBook(book)
+
     // Show alerts
     ui.showAlert('Book added', 'success');
     }
@@ -95,6 +146,9 @@ document.querySelector('#book-list').addEventListener('click', function(e){
 
     // delete book
     ui.deleteBook(e.target)
+
+    // remove from local storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
     // Show alert
     ui.showAlert('Book deleted', 'success')
